3a7cdc6762fc09705ea77757dba600aa89bb1bab
John Okoroafor <john.okoroafor@konga.com>
2018-08-18 15:41:58 +0100

    Created Master Detail Page

---
+++ MasterDetail/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MasterDetail
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            Master = new MasterPage();
+            Detail = new NavigationPage(new DetailPage());
+            MasterPage.MenuListView.ItemSelected += MenuListView_ItemSelected;
+
+            InitializeComponent();
+        }
+
+
+        void MenuListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var listView = sender as ListView;
+            var menuItem = listView.SelectedItem as MenuItem;
+
+            var page = (Page)Activator.CreateInstance(menuItem.TargeType);
+            Detail = new NavigationPage(page);
+            IsPresented = false;
+        }
+
+    }
+}
---
+++ MasterDetail/MasterPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MasterDetail
+{
+    public partial class MasterPage : ContentPage
+    {
+        static ListView menuListView;
+
+        public static ListView MenuListView { get { return menuListView; } }
+        public MasterPage()
+        {
+            InitializeComponent();
+            MasterList.ItemsSource = new List<MenuItem>()
+            {
+                new MenuItem {
+                    Name = "First Page",
+                    Icon = "Icon",
+                    TargeType = typeof(DetailPage)
+                },
+                new MenuItem {
+                    Name = "Second Page",
+                    Icon = "Icon",
+                    TargeType = typeof(SecondPage)
+                },
+            };
+
+            menuListView = MasterList;
+
+        }
+    }
+
+    public class MenuItem {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public Type TargeType { get; set; }
+    }
+}
